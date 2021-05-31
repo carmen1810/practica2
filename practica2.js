4adbb11729f68c96d059cca989144127020a0fdd
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-05-31 15:45:35 -0500

    Update practica2.js

--- practica2.js
+++ practica2.js
@@ -1,1 +1,13 @@
+//EJERCICIO 1//
 
+let John={ name: "John", surname: "Smith", id: 1 };
+let Pete = { name: "Pete", surname: "Hunt", id: 2 };
+let Mary = { name: "Mary", surname: "Key", id: 3 };
+let users = [ John, Pete, Mary ];
+console.log(users)
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+console.log( usersMapped);
+//EJERCICIO 2///
