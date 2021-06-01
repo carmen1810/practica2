2d0d924b88a877b1074e88362705afe8d9f47484
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-05-31 21:08:59 -0500

    Update practica2.js

--- practica2.js
+++ practica2.js
@@ -30,3 +30,14 @@ console.log(styles);
 styles.unshift("Rap", "Reggae")
 console.log(styles);
 //EJERCICIO 4//
+
+function filterRange(arr, a, b) {
+  // added brackets around the expression for better readability
+  return arr.filter(item => (a <= item && item <= b));
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered);
+console.log(arr)
+//EJERCICIO 5//
